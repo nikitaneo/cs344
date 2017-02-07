448a5d16b119e2f5e05e7a4f48b9066f34dd008b
Nikita <sergeevniks@ya.ru>
2017-02-08 01:18:31 +0300

    Add solution for PS5

--- Problem Sets/Problem Set 5/student.cu
+++ Problem Sets/Problem Set 5/student.cu
@@ -26,18 +26,32 @@
 
 
 #include "utils.h"
+#include <stdio.h>
 
 __global__
-void yourHisto(const unsigned int* const vals, //INPUT
+void histogram(const unsigned int* const vals, //INPUT
                unsigned int* const histo,      //OUPUT
-               int numVals)
+               const unsigned int numVals)
 {
-  //TODO fill in this kernel to calculate the histogram
-  //as quickly as possible
+  extern __shared__ unsigned int subhist[];
 
-  //Although we provide only one kernel skeleton,
-  //feel free to use more if it will help you
-  //write faster code
+  const unsigned int idx = threadIdx.x + blockDim.x * blockIdx.x;
+  const unsigned int tid = threadIdx.x;
+
+  subhist[4 * tid] = 0;
+  subhist[4 * tid + 1] = 0;
+  subhist[4 * tid + 2] = 0;
+  subhist[4 * tid + 3] = 0;
+  __syncthreads();
+
+  for(int i = 0; i < 8; ++i)
+      atomicAdd(&(subhist[vals[8 * idx + i]]), 1);
+  __syncthreads();
+
+  atomicAdd(&(histo[4 * tid]), subhist[4 * tid]);
+  atomicAdd(&(histo[4 * tid + 1]), subhist[4 * tid + 1]);
+  atomicAdd(&(histo[4 * tid + 2]), subhist[4 * tid + 2]);
+  atomicAdd(&(histo[4 * tid + 3]), subhist[4 * tid + 3]);
 }
 
 void computeHistogram(const unsigned int* const d_vals, //INPUT
@@ -45,10 +59,9 @@ void computeHistogram(const unsigned int* const d_vals, //INPUT
                       const unsigned int numBins,
                       const unsigned int numElems)
 {
-  //TODO Launch the yourHisto kernel
+  const unsigned int threads = 256;
+  const unsigned int blocks = numElems / threads / 8;
 
-  //if you want to use/launch more than one kernel,
-  //feel free
-
+  histogram<<<blocks, threads, numBins * sizeof(unsigned int)>>>(d_vals, d_histo, numElems);
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
 }
